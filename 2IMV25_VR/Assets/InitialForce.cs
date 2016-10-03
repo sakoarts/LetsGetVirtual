a336186fef3273d76a1f3f4a24b055e26f8de899
Sako Arts <sakoarts@gmail.com>
2016-10-03 10:54:27 +0200

    Initial report

---
+++ 2IMV25_VR/Assets/InitialForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialForce : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float thrust;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		//rb.AddForce (new Vector3(thrust, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rb.AddForce (new Vector3( Random.Range(-100.0F, 100.0F),  0,  Random.Range(-100.0F, 100.0F)));
+	}
+}
