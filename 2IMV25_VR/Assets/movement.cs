291e647c77404982fafcb84567d54e32b8ecb695
Stefan Luijten <sjaluijten@gmail.com>
2016-10-04 10:26:10 +0200

    Revert "Aanpassinen ProblemSet.cs (bugfixes)"
    This reverts commit 163ac35c00461f95a0e663a21b0bfc04442bdcba.

---
+++ 2IMV25_VR/Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	float speedX = 0;
+	float speedY = -5;
+	float speedZ = 0;
+	bool pressed = false;
+	public float depth = -10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.T)){
+			pressed = true;
+		}
+		if (pressed) {
+			float pos = this.transform.position.y;
+			if (pos > depth) {
+				transform.Translate (new Vector3 (speedX, speedY, speedZ) * Time.deltaTime);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2IMV25_VR
+for the course 2IMV25 interactive virtual enviroments we will create a virtual enviromnent using the oculus
